f2f5f93b273f8dbf3beaf2fead2a5763eeecb941
sandip singh <sandip.03934@gmail.com>
2019-09-16 22:54:47 +0530

    The Skeleton Spring Boot - Angular App.

---
+++ employee-mgmt-app/src/main/java/com/sandy03934/employeeportal/com/sandy03934/employeeportal/controller/ApplicationController.java
@@ -0,0 +1,27 @@
+package com.sandy03934.employeeportal.com.sandy03934.employeeportal.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApplicationController implements ErrorController {
+
+    private static final String LANDING_PAGE = "index.html";
+
+    private static final String ERROR_PATH = "/error";
+
+    @RequestMapping("/")
+    public String home() {
+        return LANDING_PAGE;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    public String error(){
+        return "forward:".concat(LANDING_PAGE);
+    }
+}
